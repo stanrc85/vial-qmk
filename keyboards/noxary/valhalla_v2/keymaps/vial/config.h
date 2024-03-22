5a6c36091ce68f3e018465621fe8f74da3c8d153
stanrc85 <stanrc@gmail.com>
2024-03-22 06:09:19 -0400

    port valhalla v2 to vial

---
+++ keyboards/noxary/valhalla_v2/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x16, 0x25, 0xBD, 0xDB, 0x3E, 0x01, 0x0B, 0x1F}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 12 }
