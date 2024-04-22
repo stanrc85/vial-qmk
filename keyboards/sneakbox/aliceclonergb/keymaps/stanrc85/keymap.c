9c7e476bdce6eb5b326dd23cf678247c66e686b7
stanrc85 <stanrc@gmail.com>
2024-04-21 21:45:52 -0400

    fix led startup fanfare

--- keyboards/sneakbox/aliceclonergb/keymaps/stanrc85/keymap.c
+++ keyboards/sneakbox/aliceclonergb/keymaps/stanrc85/keymap.c
@@ -110,10 +110,9 @@ bool led_update_user(led_t led_state) {
 
 //Startup fanfare
 
-static uint8_t top = 1;
-static uint8_t middle = 1;
-static uint8_t bottom = 1;
-
+static uint8_t top = 0;
+static uint8_t middle = 0;
+static uint8_t bottom = 0;
 
 static bool is_enabled = true;
 static bool is_rgblight_startup = true;
@@ -122,31 +121,27 @@ static uint16_t rgblight_startup_loop_timer;
 void matrix_scan_user(void) {
     // Boot up "fanfare"
     if (is_rgblight_startup && is_keyboard_master()) {
-        writePin(INDICATOR_PIN_0, top);
-        writePin(INDICATOR_PIN_1, middle);
-        writePin(INDICATOR_PIN_2, bottom);
-        if (timer_elapsed(rgblight_startup_loop_timer) > 30) {
+        if (timer_elapsed(rgblight_startup_loop_timer) > 10) {
             static uint8_t counter;
-
             counter++;
             if (counter == 1) {
                 top = 1;
-                //writePin(INDICATOR_PIN_0, top);
-                wait_ms(500);
+                writePin(INDICATOR_PIN_0, top);
+                wait_ms(300);
                 top = 0;
                 writePin(INDICATOR_PIN_0, top);
             }
             if (counter == 2) {
                 middle = 1;
-                //writePin(INDICATOR_PIN_1, middle);
-                wait_ms(500);
+                writePin(INDICATOR_PIN_1, middle);
+                wait_ms(300);
                 middle = 0;
                 writePin(INDICATOR_PIN_1, middle);
             }
             if (counter == 3) {
                 bottom = 1;
-                //writePin(INDICATOR_PIN_2, bottom);
-                wait_ms(500);
+                writePin(INDICATOR_PIN_2, bottom);
+                wait_ms(300);
                 bottom = 0;
                 writePin(INDICATOR_PIN_2, bottom);
             }
