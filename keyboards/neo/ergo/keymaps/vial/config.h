791682cac1fd4c2aea569a531a5b32f5129ba8a9
stanrc85 <stanrc@gmail.com>
2024-06-30 18:01:03 -0400

    neo ergo vial port

---
+++ keyboards/neo/ergo/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 14 }
+#define VIAL_KEYBOARD_UID {0x0C, 0x5E, 0x76, 0x6A, 0xEE, 0xB8, 0x08, 0x29}
+
