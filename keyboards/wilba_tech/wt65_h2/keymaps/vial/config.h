b75068d2fa01b0150ddb8b12ddebbfee7db5066f
stanrc85 <stanrc@gmail.com>
2024-08-30 09:56:46 -0400

    bauer lite vial changes

--- keyboards/wilba_tech/wt65_h2/keymaps/vial/config.h
+++ keyboards/wilba_tech/wt65_h2/keymaps/vial/config.h
@@ -4,5 +4,5 @@
 
 #define VIAL_KEYBOARD_UID {0x2E, 0xBE, 0xCC, 0x0B, 0xB1, 0xA8, 0x77, 0x4E}
 
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 12 }
